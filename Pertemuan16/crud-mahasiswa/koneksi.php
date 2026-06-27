c477314ba393355369263c1bf150bc9e8bcfcd89
agus kurniawan <aguskurniawann212@gmail.com>
2026-06-27 21:41:33 +0700

    Add files via upload

---
+++ Pertemuan16/crud-mahasiswa/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";     // Default XAMPP
+$pass = "root";         // Default XAMPP (kosong)
+$db   = "db_mahasiswa";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Cek apakah koneksi berhasil
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+?>
+
+
